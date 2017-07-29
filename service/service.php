f8ec1eff207e108d277c3b97be278bdf69ee80e3
WestCoast7 <sherzod-1993@list.ru>
2017-07-29 11:37:49 +0500

    Initial commit

---
+++ service/service.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Наши услуги</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  	<script src="../js/bootstrap.min.js"></script>
+</head>
+<body>
+
+</body>
+</html>
+	
+<!--Navigation-->
+<div class="container-fluid">
+	
+<?php //include("../html/nav.html"); ?>
+</div>
+<!---->
+<div class="container-fluid" style="padding: 8px 20px;">
+	<div class="row">
+		<img src="img/webR.jpg" alt="razrabotka"  style="width:100%">
+		<div class="caption" style="text-align: center;">
+		    <h3>Разработка веб сайтов</h3>
+		</div>
+	</div>
+
+	<div class="row">
+		<img src="img/market.jpg" alt="market"  style="width:100%">
+		<div class="caption" style="text-align: center;">
+		    <h3>Создание интернет-магазина</h3>
+		</div>
+	</div>
+</div>
